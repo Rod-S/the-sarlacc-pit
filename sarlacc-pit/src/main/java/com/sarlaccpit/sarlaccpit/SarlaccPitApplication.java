f06fed24b902e8a58e8840f74583b7eadd6cd349
Rod-S <rodsanati@gmail.com>
2021-05-01 23:25:53 -0500

    add spring initializr

---
+++ sarlacc-pit/src/main/java/com/sarlaccpit/sarlaccpit/SarlaccPitApplication.java
@@ -0,0 +1,13 @@
+package com.sarlaccpit.sarlaccpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarlaccPitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarlaccPitApplication.class, args);
+    }
+
+}
---
+++ sarlacc-pit/src/test/java/com/sarlaccpit/sarlaccpit/SarlaccPitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarlaccpit.sarlaccpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarlaccPitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
